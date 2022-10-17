d188c9634ef4e41e66731bc0fbeb25b399ca1fc7
GedIrina <gedirina@mail.ru>
2022-10-17 22:06:35 +0300

    задача4

---
+++ task3/Program.cs
@@ -0,0 +1,8 @@
+﻿int i=int.Parse(Console.Readline());
+if (i%2==0) 
+{
+    Console.Writeline("Да");
+}
+else {
+    Console.Writeline("Нет");
+}
---
+++ tasks4/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = int.Parse(Console.Readline());
+if (n>0) {
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 == 0) {
+            Console.Writeline(i);
+        }
+    }
+} else if (n < 0)
+{
+    for (int i = 1; i>= n; i--) {
+        if (i % 2 == 0 && i=0) {
+            Console.Writeline(i);
+        }
+    }
+}
