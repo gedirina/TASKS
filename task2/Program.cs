58474e93b5b9bbbcdc0ce6e739381b0270791c47
GedIrina <gedirina@mail.ru>
2022-10-17 21:48:59 +0300

    задача3

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿int i=int.Parse(Console.ReadLine());
+int j=int.Parse(Console.ReadLine());
+int k=int.Parse(Console.ReadLine());
+int max=1;
+if (j > max)
+{
+    max=j;
+}
+if (k > max)
+{
+    max=k;
+}
+
+Console.WriteLine(max);
